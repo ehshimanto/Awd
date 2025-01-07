9f72ebd9a5742d2a3b3d779f36186df76e54df35
ehshimanto <ehshimanto1111@gmail.com>
2025-01-07 21:02:16 +0600

    forgot password

--- Admin_Panel/reset-password.php
+++ Admin_Panel/reset-password.php
@@ -16,40 +16,37 @@ include_once "file_function/function.php";
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
-                  <h3 class="mb-3">forgot-password</h3>
+                  <h3 class="mb-3">Reset Password</h3>
                     <div class="bg-white shadow rounded">
                         <div class="row">
                             <div class="col-md-7 pe-0">
                                 <div class="form-left h-100 py-5 px-5">
-                                    <?php
-                                    if($_POST){
-                                        $email=$_POST['email'];
-                                        $sel="SELECT * FROM students WHERE st_email='$email'";
-                                        $QR=mysqli_query($connect,$sel);
-                                        $data=mysqli_fetch_array($QR);
-                                        if($data){
-                                            header("Location:reset-password.php");
-                                        }else{
-                                            echo"This Email Did Not Match";
-                                        }
-                                    }
-                                    ?>
+                                    
                                     <form action="" method="post" class="row g-4">
                                         <div class="col-12">
-                                            <label>Email<span class="text-danger">*</span></label>
+                                            <label>New Password<span class="text-danger">*</span></label>
                                             <div class="input-group">
-                                                <div class="input-group-text"><i class="fas fa-envelope"></i></div>
-                                                <input type="email" name="email" class="form-control" placeholder="Enter Email">
+                                                <div class="input-group-text"><i class="fas fa-lock"></i></div>
+                                                <input type="password" name="newpass" class="form-control" placeholder="Enter password">
                                             </div>
                                         </div>
-                                      
-                                        <div class="col-sm-6">
-                                            <a href="login.php" class="float-end text-primary">Back</a>
+
+                                        <div class="col-12">
+                                            <label>Comfarm Password<span class="text-danger">*</span></label>
+                                            <div class="input-group">
+                                                <div class="input-group-text"><i class="fas fa-lock"></i></div>
+                                                <input type="password" name="cpass"  class="form-control" placeholder="Enter Password">
+                                            </div>
                                         </div>
-                                                                               
+
+                                        
+
+                                        <div class="col-sm-6">
+                                            <a href="forgot-password.php" class="float-end text-primary">Back</a>
+                                        </div>
 
                                         <div class="col-12 text-center">
-                                            <button type="submit" class="btn btn-primary px-4 mt-4">send</button>
+                                            <button type="submit" class="btn btn-primary px-4  mt-4">Reset Password</button>
                                         </div>
                                     </form>
                                 </div>
